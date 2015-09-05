923656f28c8b5110087350df41e5b93c5ff3bcb5
kg <king_ganesh@hotmail.com>
2015-09-05 00:25:16 -0700

    helloworld

---
+++ runbooks/HelloWorld.ps1
@@ -0,0 +1,8 @@
+﻿workflow HelloWorld
+{
+    Get-Date
+    Write-Progress "Hello World Progress"
+    Write-Output "Hello World Output"
+    Write-Warning "Hello World Warning"
+    Write-Error "Hello World Error"
+}
