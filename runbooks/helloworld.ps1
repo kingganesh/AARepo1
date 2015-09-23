3e215349134736795df7e08dc703377abb2bf762
KG <king_ganesh@hotmail.com>
2015-09-23 16:11:41 -0700

    Created helloworld.ps1 in AzureAutomation.

---
+++ runbooks/helloworld.ps1
@@ -0,0 +1,8 @@
+workflow helloworld
+{
+	get-date
+    Write-Progress "Hello World Progress"
+    Write-Output "Hello World Output"
+    Write-Warning "Hello World Warning"
+    Write-Error "Hello World Error"
+}
